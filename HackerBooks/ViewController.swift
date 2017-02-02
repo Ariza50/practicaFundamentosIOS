42faf373faa292b12578fc91d0ac610e6efb2d11
Ariz <vicente@arizsystems.es>
2017-02-02 20:20:57 +0100

    Initial Commit

---
+++ HackerBooks/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HackerBooks
+//
+//  Created by Vicente Albert López on 2/2/17.
+//  Copyright © 2017 Ariz Software, S.L. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
